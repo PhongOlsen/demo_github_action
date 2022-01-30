737418552ccd49139152cc9b35614e20470cedbb
la phong <laphong@MacBook-Pro-cua-la.local>
2022-01-31 03:03:08 +0700

    update

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Mai Linh</h1>
-        <h1>Mai Linh</h1>
       </header>
     </div>
   );
